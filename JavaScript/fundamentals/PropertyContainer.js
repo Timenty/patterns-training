bd4744872ebaa5ad237046127e93fd48a2524afd
Timenty <sporretimmur@gmail.com>
2021-02-21 09:29:34 +0600

    added examples in PropertyContainer

--- JavaScript/fundamentals/PropertyContainer.js
+++ JavaScript/fundamentals/PropertyContainer.js
@@ -1,3 +1,10 @@
+/*
+    Property Container (контейнер свойств) - шаблон фундаментальный.
+    Его функция - обеспечить, чтобы приложение, уже построенное и развернутое,
+    могло динамически расширяться.
+
+    т.е мы можем добавлять динамически новые поля в наш существующий объект
+*/
 class PropertyContainer {
 
     #propertyContainer = {};
@@ -31,3 +38,112 @@ class PropertyContainer {
         return this;
     }
 }
+
+//  use through inheritance
+class UserInheritance extends PropertyContainer {
+    #name = 'Anonimus';
+
+    /**
+     * @param  {string} name=''
+     */
+    constructor(name = '') {
+        this.#name = name;
+    }
+
+    /**
+     * @param  {} name=''
+     */
+    set name(name = '') {
+        this.#name = name;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get name() {
+        return this.#name;
+    }
+}
+
+// USAGE
+/*
+    const userInheritance = new UserInheritance('Vasilyi');
+    userInheritance.setProperty('cardId', 25);
+    userInheritance.getProperty('cardId'); // 25
+*/
+
+
+
+// use through composition
+
+class UserComposition {
+
+    #name = 'Anonimus';
+    propertyContainer;
+
+    /**
+     * @param  {string} name=''
+     */
+    constructor(name = '') {
+        this.#name = name;
+        this.propertyContainer = new PropertyContainer();
+    }
+
+    /**
+     * @param  {} name=''
+     */
+    set name(name = '') {
+        this.#name = name;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get name() {
+        return this.#name;
+    }
+}
+
+// USAGE
+/*
+    const userComposition = new UserComposition('Vasilyi');
+    userComposition.propertyContainer.setProperty('cardId', 25);
+    userComposition.propertyContainer.getProperty('cardId'); // 25
+*/
+
+
+
+class UserAggregation {
+
+    #name = 'Anonimus';
+    propertyContainer;
+
+    /**
+     * @param  {string} name=''
+     */
+    constructor(name = '', PropertyContainer) {
+        this.#name = name;
+        this.propertyContainer = PropertyContainer;
+    }
+
+    /**
+     * @param  {} name=''
+     */
+    set name(name = '') {
+        this.#name = name;
+    }
+
+    /**
+     * @returns {string}
+     */
+    get name() {
+        return this.#name;
+    }
+}
+
+// USAGE
+/*
+    const userAggregation = new UserAggregation('Vasilyi', new PropertyContainer());
+    userAggregation.propertyContainer.setProperty('cardId', 25);
+    userAggregation.propertyContainer.getProperty('cardId'); // 25
+*/
